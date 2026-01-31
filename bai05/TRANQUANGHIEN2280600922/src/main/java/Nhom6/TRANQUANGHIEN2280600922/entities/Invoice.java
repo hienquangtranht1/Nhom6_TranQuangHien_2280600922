f85270f8928aa24a7d977023569b394689a3c50c
hienquangtranht1 <hienquangtranht1@gmail.com>
2026-01-31 14:35:51 +0700

    bai04

---
+++ bai05/TRANQUANGHIEN2280600922/src/main/java/Nhom6/TRANQUANGHIEN2280600922/entities/Invoice.java
@@ -0,0 +1,48 @@
+package Nhom6.TRANQUANGHIEN2280600922.entities;
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.Hibernate;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import java.util.Objects;
+
+@Getter
+@Setter
+@ToString
+@RequiredArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+@Table(name = "invoices")
+public class Invoice {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "invoice_date")
+    private Date invoiceDate = new Date();
+
+    @Column(name = "total")
+    // @Positive(message = "Total must be positive") // Tạm đóng để test
+    private Double price;
+
+    @OneToMany(mappedBy = "invoice", cascade = CascadeType.ALL)
+    @ToString.Exclude
+    private List<ItemInvoice> itemInvoices = new ArrayList<>();
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || Hibernate.getClass(this) != Hibernate.getClass(o)) return false;
+        Invoice invoice = (Invoice) o;
+        return getId() != null && Objects.equals(getId(), invoice.getId());
+    }
+
+    @Override
+    public int hashCode() {
+        return getClass().hashCode();
+    }
+}
