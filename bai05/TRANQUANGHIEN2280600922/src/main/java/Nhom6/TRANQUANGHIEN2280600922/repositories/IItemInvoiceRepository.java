26a62af562542dd81ef058783d7bfafdfb73cc21
Trần Quang Hiển <160805012+hienquangtranht1@users.noreply.github.com>
2026-01-31 14:38:45 +0700

    Delete bai05/TRANQUANGHIEN2280600922 directory

--- bai05/TRANQUANGHIEN2280600922/src/main/java/Nhom6/TRANQUANGHIEN2280600922/repositories/IItemInvoiceRepository.java
+++
@@ -1,9 +0,0 @@
-package Nhom6.TRANQUANGHIEN2280600922.repositories;
-
-import Nhom6.TRANQUANGHIEN2280600922.entities.ItemInvoice;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface IItemInvoiceRepository extends JpaRepository<ItemInvoice, Long>{
-}
